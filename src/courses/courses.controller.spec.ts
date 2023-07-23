fb97079460b0b2eba239172933242c2114733357
santo-ti <kalangoti@gmail.com>
2023-07-23 16:08:22 -0300

    test: Alter Test to CoursesController

--- src/courses/courses.controller.spec.ts
+++ src/courses/courses.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoursesController } from './courses.controller';
+import { CoursesService } from './courses.service';
 
 describe('CoursesController', () => {
   let controller: CoursesController;
@@ -7,6 +8,7 @@ describe('CoursesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CoursesController],
+      providers: [CoursesService],
     }).compile();
 
     controller = module.get<CoursesController>(CoursesController);
